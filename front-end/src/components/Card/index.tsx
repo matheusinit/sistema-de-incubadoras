7f7111c5dd55ed65c1c6e3f022afbdedc30fb1e0
matheusinit <matheus.oliveira.s@protonmail.com>
2022-05-22 09:42:49 -0300

    change background color of card

--- front-end/src/components/Card/index.tsx
+++ front-end/src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card: React.FC = () => {
   return (
-    <div className="ps-3 pe-5 pt-3 pb-3 border rounded bg-light">
+    <div className="ps-3 pe-5 pt-3 pb-3 border rounded bg-white">
       <div className="fw-light text-muted">Faturamento no último ano</div>
       <div className="fs-1">R$ 25.000</div>
     </div>
